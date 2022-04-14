d566862d49e4abaef721f891c0d1455f04713dc0
BuildTools <unconfigured@null.spigotmc.org>
2022-04-14 11:33:57 -0300

    Uploading changes before formatting

--- src/main/kotlin/filemanagement/SafeConfigManager.kt
+++ src/main/kotlin/filemanagement/SafeConfigManager.kt
@@ -4,7 +4,6 @@ import org.bukkit.Bukkit
 import org.bukkit.configuration.file.FileConfiguration
 import org.bukkit.configuration.file.YamlConfiguration
 import java.io.File
-import java.io.InputStream
 
 abstract class SafeConfigManager(fileName: String, pluginName: String) {
     private val file: File
@@ -12,7 +11,7 @@ abstract class SafeConfigManager(fileName: String, pluginName: String) {
     var config: FileConfiguration
 
     init {
-        file = File(Bukkit.getServer().pluginManager.getPlugin(pluginName)?.dataFolder, fileName)
+        file = File(Bukkit.getServer().pluginManager.getPlugin(pluginName)?.dataFolder, if(fileName.endsWith("yml")) fileName else "$fileName.yml")
         if (!file.exists())
             file.createNewFile()
         config = loadCustomFile(this.file)
